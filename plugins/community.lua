92ad3c1d981841e3b688fcea6514bb521e367ebd
Oli Zimpasser <oglimmer@gmail.com>
2023-04-14 23:27:15 +0200

    configure jdtls

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,7 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  "mfussenegger/nvim-jdtls",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
